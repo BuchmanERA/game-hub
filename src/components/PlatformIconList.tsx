956fe4d0d5587205d0cbe4d9cb0c403f19b4975a
Joel Buchman <j.buchman@erasolutions.us>
2024-05-04 22:05:32 -0500

    npm not installing icons, quick save to restart.

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,44 +1,42 @@
-import React from 'react'
 import {
     FaWindows,
     FaPlaystation,
     FaXbox,
     FaApple,
     FaLinux,
-    FaAndroid
-} from 'react-icons/fa'
+    FaAndroid,
+} from "react-icons/fa";
 import { MdPhoneIphone } from 'react-icons/md';
 import { SiNintendo } from 'react-icons/si';
-import { BsGlobe } from 'react-icons/bs'
-import { HStack, Icon, Text } from '@chakra-ui/react'
+import { BsGlobe } from 'react-icons/bs';
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
-import { Platform } from '../hooks/useGame'
-
 
 interface Props {
-    platforms: Platform[]
+    platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms = [] }: Props) => {
-  const iconMap: { [key: string]: IconType } = { 
-    pc: FaWindows,
-    playstation: FaPlaystation,
-    xbox: FaXbox,
-    nintendo: SiNintendo,
-    mac: FaApple,
-    linux: FaLinux, 
-    android: FaAndroid,
-    ios: MdPhoneIphone,
-    web: BsGlobe
-  }
+    const iconMap: { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+    }
+
     return (
-        <HStack marginY={2}>
-            {platforms.map((platforms) => (
-                <Icon key={platforms.id} as={iconMap[platforms.slug]} color='gray.500' />
+        <HStack marginY={1}>
+            {platforms.map((platform) => (
+                <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' />
             ))}
-
         </HStack>
-    )
-}
+    );
+};
 
-export default PlatformIconList
+export default PlatformIconList;
