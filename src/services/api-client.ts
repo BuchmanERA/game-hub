1e813846c2d1b986351af5b9ca1c1b928066be07
Joel Buchman <137914427+BuchmanERA@users.noreply.github.com>
2024-05-08 18:57:11 -0500

    new api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "a01dfd3de48448b9a0001134418ee41d",
+    key: "40dd7886ac0043cfaf088a618f0a4479",
   },
 });
